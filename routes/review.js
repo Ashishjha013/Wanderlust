7ba562fce6e9613d4fd66f0df1927bffb29cd249
Ashishjha013 <ashishjha1304@gmail.com>
2025-09-18 11:11:10 +0530

    chore: initial commit - Wanderlust project

---
+++ routes/review.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const wrapAsync = require("../utils/wrapAsync.js");
+const ExpressError = require("../utils/ExpressError.js");
+const { reviewSchema } = require("../schema.js");
+const Review = require("../models/review.js");
+const Listing = require("../models/listing.js");
+
+// Validation middleware for reviews
+const validateReview = (req, res, next) => {
+  let { error } = reviewSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(", ");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
+//Reviews
+//Post Route
+router.post(
+  "/",
+  validateReview,
+  wrapAsync(async (req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+
+    req.flash("success", "New Review Created!");
+    res.redirect(`/listings/${listing._id}`);
+  })
+);
+
+//Delete Review Route
+router.delete(
+  "/:reviewId",
+  wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
+
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+
+    req.flash("success", "Review Deleted Successfully!");
+    res.redirect(`/listings/${id}`);
+  })
+);
+
+// Export router → makes routes in this file reusable in other files via require()
+module.exports = router;
